c29ab7162c0d7b0986ff18b057b11609558bdae7
Fabian Lancheros <fabian1423ft@gmail.com>
2026-05-05 17:11:18 -0500

    5/5/2026 5:11 GEOLINK

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>GEOLIK REGISTRO</title>
+</head>
+<body>
+
+    <div style="display: flex; justify-content: center; margin-top: 10%;">
+        <div class="cuadroregistro">
+            <div style="width: 90%; justify-items: center">
+                <div style="width: 50%">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="usuario" placeholder="Usuario">
+                        <label for="usuario">Usuario</label>
+                    </div>
+                </div>
+                <div style="width: 50%">
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="Password" placeholder="Password">
+                        <label for="Password">Password</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--<a href="{{route('welcome')}}">link</a> forma de llamar a un hipervinculo dentro de laravel -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+Route::get('/', function () {
+    return view('registro');
 });
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
+
+
