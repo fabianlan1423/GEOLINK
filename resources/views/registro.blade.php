f25d604e0cf1e0e0e8508c7d206cb5fa9dd031bd
Fabian Lancheros <fabian1423ft@gmail.com>
2026-05-08 10:26:59 -0500

    8/5/2026  10:26 GEOLINK

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -8,23 +8,29 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
     <title>GEOLIK REGISTRO</title>
 </head>
-<body>
+<body class="bod">
 
     <div style="display: flex; justify-content: center; margin-top: 10%;">
         <div class="cuadroregistro">
-            <div style="width: 90%; justify-items: center">
-                <div style="width: 50%">
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="usuario" placeholder="Usuario">
-                        <label for="usuario">Usuario</label>
+            <div class="cuadro50porcen"><!--INGRESO-->
+                <div style="width: 90%; justify-items: center; align-content: center;">
+                    <div style="width: 50%">
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="usuario" placeholder="Usuario">
+                            <label for="usuario">Usuario</label>
+                        </div>
                     </div>
-                </div>
-                <div style="width: 50%">
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="Password" placeholder="Password">
-                        <label for="Password">Password</label>
+                    <div style="width: 50%">
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="Password" placeholder="Password">
+                            <label for="Password">Password</label>
+                        </div>
                     </div>
+
                 </div>
+            </div>
+            <div class="cuadro50porcen"><!--REGISTRO USUARIO-->
+
             </div>
         </div>
     </div>
